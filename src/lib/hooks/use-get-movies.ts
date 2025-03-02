1903f4d7b5e341180ac0e8d95e92432608f52dc3
Amjad Dahdoul <a.dahdoul.ext@work4all.de>
2025-03-02 23:39:42 +0100

    Add URL_API

--- src/lib/hooks/use-get-movies.ts
+++ src/lib/hooks/use-get-movies.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const URL_API = process.env.URL_API;
+const URL_API = "https://imdb.iamidiotareyoutoo.com/search?q=";
 
 export const useGetMovies = (searchQuery: string) => {
   const [movies, setMovies] = useState([]);
