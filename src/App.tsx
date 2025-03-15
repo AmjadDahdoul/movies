5261c0bbbfdcc8265fb451870ae7a88f32695891
Amjad Dahdoul <amjad.dahdoul@gmail.com>
2025-03-15 04:46:34 +0100

    Rename to HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { BrowserRouter } from "react-router";
 import "./App.css";
 import { ThemeProvider } from "./lib/theme/ThemeContextProvider";
 import { Layout } from "./lib/components/layout/Layout";
-import { MoviesListLayout } from "./lib/components/movie-list/MoviesListLayout";
+import { HomePage } from "./lib/pages/HomePage";
 
 function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
         <Layout title='Tranquility'>
-          <MoviesListLayout />
+          <HomePage />
         </Layout>
       </BrowserRouter>
     </ThemeProvider>
