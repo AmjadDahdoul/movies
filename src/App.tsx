f5a6f8991e5344f3fe938bed3c5fe281c30ef4d4
Amjad Dahdoul <amjad.dahdoul@gmail.com>
2025-06-14 00:53:04 +0200

    Splash Cursor

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,38 @@ import "./App.css";
 import { ThemeProvider } from "./lib/theme/ThemeContextProvider";
 import { Layout } from "./lib/components/layout/Layout";
 import { HomePage } from "./lib/pages/HomePage";
+import SplashCursor from "./lib/components/SplashCursor";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [showSplashCursor, setShowSplashCursor] = useState(false);
+  const [typedKeys, setTypedKeys] = useState("");
+
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      const newTypedKeys = (typedKeys + event.key).toLowerCase();
+      setTypedKeys(newTypedKeys);
+
+      if (newTypedKeys.includes("fabulous")) {
+        setShowSplashCursor(!showSplashCursor);
+        setTypedKeys("");
+      } else if (newTypedKeys.length > 10) {
+        setTypedKeys(newTypedKeys.slice(-10));
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [showSplashCursor, typedKeys]);
+
   return (
     <ThemeProvider>
       <BrowserRouter>
         <Layout title='Tranquility'>
+          {showSplashCursor && <SplashCursor />}
           <HomePage />
         </Layout>
       </BrowserRouter>
