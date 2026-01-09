0201984399d7f4f515dede35dd80c6ce44384cfb
Amjad Dahdoul <amjad.dahdoul@gmail.com>
2026-01-09 23:23:13 +0100

    Add copy button for watchlist

--- src/lib/components/watchlist/Watchlist.tsx
+++ src/lib/components/watchlist/Watchlist.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import { WatchedMoviesList } from "../../types/types";
 import { MovieGrid } from "../movie-grid/MovieGrid";
 
@@ -9,11 +9,32 @@ export const Watchlist = ({
 }) => {
   const moviesList = watchlist?.results || [];
 
+  const copyToClipboard = async () => {
+    try {
+      const watchlistText = moviesList
+        .map(
+          (movie, index) =>
+            `${index + 1}. ${movie.title} (${movie.vote_average.toPrecision(
+              2
+            )})`
+        )
+        .join("\n");
+
+      await navigator.clipboard.writeText(watchlistText);
+    } catch (error) {
+      console.error("Failed to copy watchlist to clipboard:", error);
+    }
+  };
+
   return (
     <Stack spacing={2} alignItems='center' pt={2} pb={4}>
       <Typography variant='h6' component='h5' textAlign='center'>
         Watchlist: {watchlist?.total_results || 0}
       </Typography>
+
+      <Button variant='contained' onClick={copyToClipboard}>
+        Copy
+      </Button>
 
       <MovieGrid movies={moviesList} />
     </Stack>
